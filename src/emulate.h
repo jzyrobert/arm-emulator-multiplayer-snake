2871ad8a2297e3e41fb642d8fb7c82acb5a9b60d
rzj17 <jzyrobert@googlemail.com>
2018-05-25 16:45:25 +0100

    header fixes

--- src/emulate.h
+++ src/emulate.h
@@ -3,8 +3,8 @@
 
 #ifndef PROCESSOR_FLOW_TYPEDEFS
 #define PROCESSOR_FLOW_TYPEDEFS
-typedef struct {}INSTRUCTION;
-typedef struct {}STATE;
+typedef struct instruction INSTRUCTION;
+typedef struct state STATE;
 #endif
 
 #ifndef ARM_TYPE_TYPEDEFS

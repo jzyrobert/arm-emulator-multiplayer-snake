0a45bf62c4d90aab8e469deb95c061da5dfee622
rzj17 <jzyrobert@googlemail.com>
2018-05-26 16:15:13 +0100

    evalRSB

--- src/assemble.c
+++ src/assemble.c
@@ -268,18 +268,14 @@ word evalBeq(ASSEMBLY *as, STATE *state){
 }
 
 word evalRsb(ASSEMBLY *as, STATE *state){
-    word output = 0xE0000000;
-
+    word output = 0;
     setAlwaysCond(&output);
-    byte op2 = (byte) strtol(as->tokens[2] + 1, NULL, 10);
     long rn = strtol(as->tokens[1] + 1, NULL, 10);
     long rd = strtol(as->tokens[0] + 1, NULL, 10);
-
-    output |= (0x00600000);
-    output |= (rd << 16);
-    output |= (rn << 12);
-    output |= op2;
-
+    setBits(&output, 3, 21);
+    setBits(&output, rd, 12);
+    setBits(&output, rn, 16);
+    evalOperand2(as, state, &output, 2);
     //printf("Output is %x\n", output);
     return output;
 }

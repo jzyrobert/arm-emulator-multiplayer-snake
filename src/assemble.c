f539d40f77b50844dd600faba1e8a1c70606fbed
rzj17 <jzyrobert@googlemail.com>
2018-05-28 13:09:26 +0100

    fix attempt

--- src/assemble.c
+++ src/assemble.c
@@ -130,9 +130,6 @@ void processTransfers(ASSEMBLY *as, STATE *state, word *output) {
         //Setting RN
         setBits(output,getRegNum(as->tokens[1] + 1) , 16);
         if (as->noOfTokens == 3) {
-            //removes ]\n;
-            as->tokens[2][strlen(as->tokens[2]) - 2] = '\n';
-            as->tokens[2][strlen(as->tokens[2]) - 1] = '\0';
             if (strchr(as->tokens[2], 'r') != NULL) {
                 //reg
                 setBits(output, 1, 25);

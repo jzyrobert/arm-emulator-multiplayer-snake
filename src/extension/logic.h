18eb13ffb28a4dfff5ec506863a8343dc54b5b96
Matthew Fennell <matthew.robert.fennell@gmail.com>
2018-06-01 12:07:18 +0100

    Moved all function declarations into logic.h

--- src/extension/logic.h
+++ src/extension/logic.h
@@ -1,19 +1,31 @@
+#include <stdbool.h>
+
 #ifndef GAME_LOGIC_DEFINITIONS
 #define GAME_LOGIC_DEFINITIONS
 typedef struct coordinate Coordinate;
 typedef struct cell Cell;
 typedef struct snake Snake;
 typedef struct game Game;
-#endif
+
+
+void printGame(Game *pGame);
+                                                                                
+void updateGame(Game *game);
+                                                                                
+void updateDir(int ch, Game *pGame);
+                                                                                
+void moveSnake(Game *game, Snake *theSnake, Cell *next);
+                                                                                
+void addLength(Game *game, Snake *theSnake);
+                                                                                
+bool oppositeDir(Snake *pSnake, int ch);
+                                                                                
+void moveSizeIncrease(Game *game, Snake *theSnake, Cell *next);
+                                                                                
+void addFood(Game *pGame);
+                                                                                
+void endgame(Game *game);
 
 // Initialises the random seed used to add snakes to random positions on the grid
 void initialiseRandomSeed(void);
-
-/*
-// Creates an empty game grid ready for snakes to be added when the game starts
-void buildGrid(Cell (*grid)[GRID_SIZE][GRID_SIZE]);
-
-// Adds a new snake to a random position on the board. Returns this snake
-// PRE: there must be at least one space not occupied by a snake on the board
-Snake addSnake(Cell (*grid)[GRID_SIZE][GRID_SIZE]);
- */
+#endif

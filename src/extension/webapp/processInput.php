b16f59d43dd038169624b9514c278f67e54a7f69
Matthew Fennell <mf2017@ic.ac.uk>
2018-06-06 14:49:40 +0100

    Added beginnings of a PHP script to handle webapp button clicks

---
+++ src/extension/webapp/processInput.php
@@ -0,0 +1,14 @@
+if($_SERVER['REQUEST_METHOD']==='POST') {
+  if (isset($_POST['up'])) {
+    // call up function in game
+  }
+  if (isset($_POST['down'])) {
+    // call down function in game
+  }
+  if (isset($_POST['left'])) {
+    // call left function in game
+  }
+  if (isset($_POST['right'])) {
+    // call right function in game
+  }
+}

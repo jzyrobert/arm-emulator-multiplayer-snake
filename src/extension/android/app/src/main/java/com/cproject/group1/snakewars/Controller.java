dbad48f1cbaadcf00f3dc27077997b65b0a1a2da
rzj17 <jzyrobert@googlemail.com>
2018-06-12 00:54:19 +0100

    added (not tested) functionality for connecting to a game

--- src/extension/android/app/src/main/java/com/cproject/group1/snakewars/Controller.java
+++ src/extension/android/app/src/main/java/com/cproject/group1/snakewars/Controller.java
@@ -1,7 +1,11 @@
 package com.cproject.group1.snakewars;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Controller extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class Controller extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
+        Context context = getApplicationContext();
+        Intent current = getIntent();
+        String IP = current.getStringExtra("IP");
+        int num = current.getIntExtra("Number", -1);
+        if (num == -1) {
+            Toast toast = Toast.makeText(context, "Invalid number!", Toast.LENGTH_SHORT);
+            toast.show();
+            finishActivity(0);
+        }
+        TextView textView;
+        String text = "You are Player " + num;
+        textView  = findViewById(R.id.textView);
+        textView.setText(text);
     }
 }
--- src/extension/android/app/src/main/java/com/cproject/group1/snakewars/SignIn.java
+++ src/extension/android/app/src/main/java/com/cproject/group1/snakewars/SignIn.java
@@ -1,9 +1,19 @@
 package com.cproject.group1.snakewars;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class SignIn extends AppCompatActivity {
 
@@ -14,7 +24,62 @@ public class SignIn extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
         Intent intent = new Intent(this, Controller.class);
-        startActivity(intent);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        Context context = getApplicationContext();
+        CharSequence text = "";
+        int duration = Toast.LENGTH_SHORT;
+        if (message.equals("")) {
+            text = "No IP Address entered!";
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        } else {
+            if (message.indexOf(':') > 0) {
+                try {
+                    Toast toast = Toast.makeText(context, "Connecting to: " + message, duration);
+                    toast.show();
+                    URL host = new URL(message);
+                    URLConnection connection = host.openConnection();
+                    connection.connect();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String input;
+                    boolean correctSite = false;
+                    while (((input = in.readLine()) != null)) {
+                        if (input.contains("Snake Wars")) {
+                            correctSite = true;
+                            text = "Connection successfull";
+                            toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                        }
+                        if (input.contains("Your number is")) {
+                            input = input.replaceAll("\\D+","");
+                            intent.putExtra("Number", Integer.parseInt(input));
+                            intent.putExtra("IP", message);
+                            break;
+                        }
+                    }
+                    in.close();
+                    if (correctSite) {
+                        startActivity(intent);
+                    } else {
+                        text = "Incorrect site!";
+                        toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                } catch (IOException e) {
+                    text = "Invalid connection!";
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            } else {
+                text = "No port specified!";
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        }
     }
 }

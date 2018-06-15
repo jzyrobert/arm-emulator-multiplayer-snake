86086fe1a3ff9ae17df8042576cc365be42155d1
Robert Jin <rzj17@voxel21.doc.ic.ac.uk>
2018-06-15 15:35:17 +0100

    reset demo variables

--- extension/democonfig.h
+++ extension/democonfig.h
@@ -8,13 +8,13 @@ typedef int placeholder;
 //Check length for dummy AI movement
 #define CHECK_LENGTH 200
 //Total size / percentage, eg 1 = 100%, 100 = 1%;
-#define FOOD_PERCENTAGE 100
+#define FOOD_PERCENTAGE 10
 //in ms
 #define GAME_REFRESH_SPEED 10
 #define PORT 2035
 //If 1, dead snakes become obstacles
 #define DEAD_SNAKES_ARE_OBSTACLES 0
 //If 1, games end when there is 1 player left, IF it started with more than 1 player
-#define QUICKER_MULTIPLAYER 0
+#define QUICKER_MULTIPLAYER 1
 
 #endif //ARM11_01_CONFIG_H
